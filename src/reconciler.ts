46010a185aa2b3881c512d70d2564b18fa95e417
halodong <drl990114@gmail.com>
2022-05-28 22:53:28 +0800

    chore: Add a function to build a subtree of the current fiber node‘

--- src/reconciler.ts
+++ src/reconciler.ts
@@ -1,10 +1,62 @@
-import { HostComponent } from './workTags'
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { giveTag } from './utils'
+import { SReactFiber } from './types'
 
-export function reconcileChildren (current: any, returnFiber: any, nextChildren: any): void {
-  console.log('应该构建fiber树', current, returnFiber, nextChildren)
-  const childFiber = {
-    tag: HostComponent,
-    type: nextChildren.type
+export function reconcileChildren (current: SReactFiber | null, returnFiber: SReactFiber, newChildren: any[]): void {
+  console.log('应该构建此fiber的子fiber树', current, returnFiber, newChildren)
+
+  debugger
+  let newChildIndex = 0
+  let oldFiber = returnFiber.alternate?.child
+  let prevSibling: SReactFiber | null = null
+
+  while (newChildIndex < newChildren.length || (oldFiber != null)) {
+    const newChild = newChildren[newChildIndex]
+    let newFiber: SReactFiber | null = null
+    const sameType = (oldFiber != null) && newChild && oldFiber.type === newChild.type
+    giveTag(newChild)
+    if (sameType && oldFiber != null) {
+      if (oldFiber?.alternate != null) {
+        newFiber = oldFiber.alternate
+        newFiber.props = newChild.props
+        newFiber.alternate = oldFiber
+      } else {
+        newFiber = {
+          tag: oldFiber.tag,
+          type: oldFiber.type,
+          props: newChild.props,
+          stateNode: oldFiber.stateNode,
+          return: returnFiber,
+          alternate: oldFiber
+        }
+      }
+    } else {
+      console.log('newChild,', newChild)
+      if (newChild) {
+        newFiber = {
+          tag: newChild.tag, // worktag
+          type: newChild.type,
+          props: newChild.props,
+          stateNode: null,
+          return: returnFiber
+        }
+      }
+    }
+
+    if (oldFiber != null) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (newFiber != null) {
+      if (newChildIndex === 0) {
+        returnFiber.child = newFiber
+      } else {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        prevSibling!.sibling = newFiber
+      }
+      prevSibling = newFiber
+    }
+    newChildIndex++
   }
-  returnFiber.child = childFiber
+  console.log('构建此fiber的子fiber树完成', returnFiber)
 }
