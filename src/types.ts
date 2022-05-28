643c75120ab118e3c3cec0d4741961120737aab7
halodong <drl990114@gmail.com>
2022-05-28 22:57:25 +0800

    chore: Adjust some types

--- src/types.ts
+++ src/types.ts
@@ -2,20 +2,17 @@ export type Key = string | number
 export interface SReactElement<P = any, T = string | Function> {
   type: T
   props: P
-  key: Key
-  ref?: any
 }
 
 export interface SReactFiber<P = any> {
   key?: string
-  type: string | Function
+  type?: string | Function
   tag: number
-  props: any
-  parentNode: HTMLElement
-  childNodes: any
-  node: HTMLElement
-  kids?: any
-  parent?: SReactFiber<P>
+  props?: any
+  return?: SReactFiber<P>
   sibling?: SReactFiber<P>
   child?: SReactFiber<P>
+  alternate?: SReactFiber<P> // old
+  effectTag?: string
+  stateNode?: Node | null
 }
