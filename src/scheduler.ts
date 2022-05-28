5c9d9737bee380be722a14310b98479ded0c06ff
halodong <drl990114@gmail.com>
2022-05-28 22:58:14 +0800

    Add workloop and scheduleRoot method

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,34 +1,72 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { SReactFiber } from './types'
 import { beginWork } from './beginWork'
-import { giveTag } from './utils'
+import { HostRoot } from './workTags'
 
 let workInProgress: any = null
+let workInProgressRoot: any = null
+let nextUnitOfWork: any = null
+const currentRoot: any = null
 
-export const schedule = (): void => {
+export const render = (vdom: any, node: Node): void => {
+  const rootFiber: SReactFiber = {
+    tag: HostRoot,
+    stateNode: node,
+    props: { children: [vdom] }
+  }
+  workInProgress = rootFiber
+  scheduleRoot(rootFiber)
+}
 
+export const scheduleRoot = (rootFiber: SReactFiber): void => { // {tag:TAG_ROOT,stateNode:container,props: { children: [element] }}
+  if (currentRoot?.alternate != null) {
+    // Performed at least one render
+    workInProgressRoot = currentRoot.alternate
+    workInProgressRoot.alternate = currentRoot
+    if (rootFiber != null) workInProgressRoot.props = rootFiber.props
+  } else if (currentRoot != null) {
+    if (rootFiber != null) {
+      rootFiber.alternate = currentRoot
+      workInProgressRoot = rootFiber
+    } else {
+      workInProgressRoot = {
+        ...currentRoot,
+        alternate: currentRoot
+      }
+    }
+  } else {
+    // first render
+    workInProgressRoot = rootFiber
+  }
+  nextUnitOfWork = workInProgressRoot
 }
-const performUnitOfWork = (unitOfWork: any): any => {
-  // shoule async
-  const current = unitOfWork.alternate
-  return beginWork(current, unitOfWork)
-}
-const workLoop = (): void => {
-  while (workInProgress != null) {
-    workInProgress = performUnitOfWork(workInProgress)
+const performUnitOfWork = (unitOfWorkFiber: any): any => {
+  //
+  beginWork(unitOfWorkFiber.alternate, unitOfWorkFiber)
+  if (unitOfWorkFiber.child != null) {
+    return unitOfWorkFiber.child
+  }
+
+  while (unitOfWorkFiber != null) {
+    // TODO completeUnitOfWork(unitOfWorkFiber)
+    if (unitOfWorkFiber.sibling != null) {
+      return unitOfWorkFiber.sibling
+    }
+    unitOfWorkFiber = unitOfWorkFiber.return
   }
 }
-export const scheduleUpdateOnFiber = (oldFiber: any): void => {
-  const newFiber = {
-    ...oldFiber,
-    alternate: oldFiber
-  }// 新的CounterFiber开始执行更新
-  workInProgress = newFiber
-  workLoop()
+
+// Loop processing fiber units of work
+const workLoop = (deadline: IdleDeadline): void => {
+  let shouldYield = false
+  while (nextUnitOfWork != null && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  if (nextUnitOfWork == null && workInProgressRoot != null) {
+    console.log('render阶段结束', workInProgressRoot)
+    // TODO commitRoot()
+  }
+  requestIdleCallback(workLoop, { timeout: 500 })
 }
-export const render = (fiber: any, node: Node): void => {
-  // eslint-disable-next-line no-debugger
-  debugger
-  console.log('render', fiber)
-  giveTag(fiber)
-  workInProgress = fiber
-  workLoop()
-}
+requestIdleCallback(workLoop, { timeout: 500 })
