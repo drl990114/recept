12d37708fa77776ed03b388697c9cd558a97f1dc
halodong <drl990114@gmail.com>
2022-05-28 22:55:17 +0800

    chore: Added method call for initial rendering in beginwork

--- src/beginWork.ts
+++ src/beginWork.ts
@@ -1,8 +1,9 @@
-import { mountFunctionComponent } from './dom'
+import { createDOM } from './dom'
 import { renderWithHooks } from './hooks'
 import { reconcileChildren } from './reconciler'
 import { SReactFiber } from './types'
-import { FunctionComponent } from './workTags'
+import { isArr } from './utils'
+import { FunctionComponent, HostComponent, HostRoot } from './workTags'
 
 export const beginWork = (current: SReactFiber, workInProgress: SReactFiber): any => {
   if (current != null) {
@@ -18,10 +19,16 @@ export const beginWork = (current: SReactFiber, workInProgress: SReactFiber): an
     }
   } else {
     switch (workInProgress.tag) {
+      case HostRoot:
+        return mountHostRoot(current, workInProgress)
       case FunctionComponent:
         return mountFunctionComponent(
-          workInProgress
+          current,
+          workInProgress,
+          workInProgress.type as Function
         )
+      case HostComponent:
+        return mountHost(current, workInProgress)
       default:
         break
     }
@@ -34,7 +41,32 @@ const updateFunctionComponent = (current: any, workInProgress: any, Component: a
     workInProgress,
     Component
   )
-  console.log('Component children', newChildren)
   reconcileChildren(current, workInProgress, newChildren)
   return workInProgress.child
 }
+
+const mountFunctionComponent = (current: SReactFiber | null, workInProgress: SReactFiber, Component: Function): SReactFiber | undefined => {
+  const children = wrapChild(renderWithHooks(
+    current,
+    workInProgress,
+    Component
+  ))
+  workInProgress.tag = FunctionComponent
+  reconcileChildren(current, workInProgress, children)
+  return workInProgress.child
+}
+const mountHostRoot = (current: SReactFiber|null, workInProgress: SReactFiber): void => {
+  const newChildren = wrapChild(workInProgress.props.children)// [element=<div id="A1"]
+  reconcileChildren(current, workInProgress, newChildren)
+}
+
+const mountHost = (current: SReactFiber | null, workInProgress: SReactFiber): void => {
+  if (workInProgress.stateNode == null) {
+    workInProgress.stateNode = createDOM(workInProgress)
+  }
+  const newChildren = wrapChild(workInProgress.props.children)
+  reconcileChildren(current, workInProgress, newChildren)
+}
+
+// tool
+const wrapChild = (children: any): any[] => isArr(children) ? children : [children]
