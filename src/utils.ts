fe9f426805bc0b59f815c8b9e753214de8d1af5f
halodong <drl990114@gmail.com>
2022-05-28 22:52:17 +0800

    chore: add a tool fn

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,12 @@
 import { FunctionComponent, HostComponent } from './workTags'
 
 export const giveTag = (fiber: any): any => {
-  if (fiber.tag != null) return fiber
+  if (fiber?.tag != null) return fiber
   if (typeof fiber.type === 'function') {
     fiber.tag = FunctionComponent
   } else {
     fiber.tag = HostComponent
   }
 }
+
+export const isArr = Array.isArray
