9073d035f9f27e291be80e1c718c2a906d1166d9
halodong <drl990114@gmail.com>
2022-05-29 21:48:24 +0800

    chore: feat expand childrenArray

--- src/h.ts
+++ src/h.ts
@@ -1,10 +1,21 @@
+import { ELEMENT_TEXT } from './constants'
 import { SReactElement } from './types'
+import { isArr } from './utils'
 
 export const h = (type: any, props: any = {}, ...children: any): SReactElement => {
-  console.log('createElement', children)
+  children = expand(children)
+  console.log('createElement', type, props, children)
   return createElement(type, props, children)
 }
 
+const expand = (arr: any[], target: any[] = []): any[] => {
+  arr.forEach(v => {
+    isArr(v)
+      ? expand(v, target)
+      : target.push(typeof v === 'string' ? createText(v) : v)
+  })
+  return target
+}
 const createElement = (
   type: any,
   props: any,
@@ -13,12 +24,8 @@ const createElement = (
   type,
   props: {
     ...props,
-    children: children.map((child: any) => {
-      return typeof child === 'object'
-        ? child
-        : {
-            props: { text: child, children: [] }
-          }
-    })
+    children
   }
 })
+
+export const createText = (text: string): SReactElement => ({ type: ELEMENT_TEXT, props: { text, children: [] } })
