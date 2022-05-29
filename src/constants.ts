3d35cf756d4f4db9a6fc8b9b5851be8c20352d01
halodong <drl990114@gmail.com>
2022-05-29 20:19:37 +0800

    chore: Adjustment format

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,6 @@ export const HostComponent /**          */ = 2
 export const TextComponent /**          */ = 3
 
 // effect flag
-export const PLACEMENT /** */ = Symbol.for('PLACEMENT')
-export const UPDATE /**    */ = Symbol.for('UPDATE')
-export const DELETION /**  */ = Symbol.for('DELETION')
+export const PLACEMENT /**              */ = Symbol.for('PLACEMENT')
+export const UPDATE /**                 */ = Symbol.for('UPDATE')
+export const DELETION /**               */ = Symbol.for('DELETION')
