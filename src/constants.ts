7516316fa754f92b4952119048f7b7c8ecd2b2ec
halodong <drl990114@gmail.com>
2022-05-29 21:49:11 +0800

    chore: add text element flag

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,11 @@
 export const HostRoot /**               */ = 0
 export const FunctionComponent /**      */ = 1
 export const HostComponent /**          */ = 2
-export const TextComponent /**          */ = 3
+export const TextNode /**               */ = 3
 
 // effect flag
 export const PLACEMENT /**              */ = Symbol.for('PLACEMENT')
 export const UPDATE /**                 */ = Symbol.for('UPDATE')
 export const DELETION /**               */ = Symbol.for('DELETION')
+
+export const ELEMENT_TEXT = '#text'
