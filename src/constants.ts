4fda05e2caa5f61720708683969c29744f181386
halodong <drl990114@gmail.com>
2022-05-29 20:18:48 +0800

    chore: add effect flags

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,10 @@
-export const HostRoot /**               */ = 0 // 根节点
-export const FunctionComponent /**      */ = 1 // 函数组件
-export const HostComponent /**          */ = 2 // 原生DOM节点
+// fiber worktags
+export const HostRoot /**               */ = 0
+export const FunctionComponent /**      */ = 1
+export const HostComponent /**          */ = 2
+export const TextComponent /**          */ = 3
+
+// effect flag
+export const PLACEMENT /** */ = Symbol.for('PLACEMENT')
+export const UPDATE /**    */ = Symbol.for('UPDATE')
+export const DELETION /**  */ = Symbol.for('DELETION')
