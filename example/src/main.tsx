4e76895f59ce0c34d24869cc0e17896ca185cd6b
halodong <drl990114@gmail.com>
2022-05-30 22:18:43 +0800

    chore: update example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,13 +1,18 @@
-import { h, render } from '../../src'
+import { h, render, useState } from '../../src'
 // import App from './App'
 import './index.css'
 
 export default function App() {
-  return <div key="A1">
-    <div key="B1">11</div>
-    <div key="B2">
-      <button key="C1">+</button>
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div key="A1">
+      <div key="B1">11</div>
+      <div key="B2">
+        <button key="C1" onClick={()=>setCounter(counter+1)}>+</button>
+        counter: <span>{counter}</span>
+      </div>
     </div>
-  </div>
+  )
 }
-render(<App key="root"/>, document.getElementById('root')!)
+render(<App key="root" />, document.getElementById('root')!)
