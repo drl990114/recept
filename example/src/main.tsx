ae14d59d7db8de0e0426d06750766793c7d6c831
halodong <drl990114@gmail.com>
2022-06-01 23:16:40 +0800

    chore: update example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,6 +1,19 @@
 import { h, render, useState } from '../../src'
-import App from './App'
 import './index.css'
 
+function App () {
+  const [count,setCount] = useState(0)
+
+  return <div>
+    <h3>计数器:
+      count {count >= 0 ? '>=':'<'} 0
+    </h3>
+    <button onClick={()=>setCount(count+1)}>+</button>
+    {count}
+    {count >0 && '大于0'}
+
+    <button onClick={()=>setCount(count-1)}>-</button>
+  </div>
+}
 
 render(<App key="root" />, document.getElementById('root')!)
