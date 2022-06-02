18f4f8a4006769a1bf9c0e9e0220f1c1af2f2290
halodong <drl990114@gmail.com>
2022-06-02 16:25:23 +0800

    chore: update example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,19 +1,24 @@
 import { h, render, useState } from '../../src'
 import './index.css'
 
-function App () {
-  const [count,setCount] = useState(0)
+function Counter (props: { count: any }) {
+  const [count,setCount] = useState(props.count)
 
   return <div>
     <h3>计数器:
       count {count >= 0 ? '>=':'<'} 0
     </h3>
-    <button onClick={()=>setCount(count+1)}>+</button>
+    <button key="btn1" onClick={()=>setCount(count+1)}>+</button>
     {count}
     {count >0 && '大于0'}
-
-    <button onClick={()=>setCount(count-1)}>-</button>
+    <button key="btn2"onClick={()=>setCount(count-1)}>-</button>
   </div>
 }
 
+function App () {
+  return <div>
+    <Counter count={1}/>
+    <footer>sreact</footer>
+  </div>
+}
 render(<App key="root" />, document.getElementById('root')!)
