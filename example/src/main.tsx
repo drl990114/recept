d0bb4e5700b0a7954274e444c8e1add55656a1fb
halodong <drl990114@gmail.com>
2022-06-06 22:57:28 +0800

    chore: update example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,26 +1,37 @@
 import { h, render, useState } from '../../src'
 import './index.css'
+// import React, { useEffect, useState } from 'react'
+// import { render } from 'react-dom'
 
-function Counter (props: { count: any }) {
-  const [count,setCount] = useState(props.count)
-  const [count2,setCount2] = useState(props.count)
+function Counter(props: { count: any }) {
+  const [count, setCount] = useState(props.count)
 
-  return <div>
-    <h3>计数器:
-      count {count >= 0 ? '>=':'<'} 0
-      <h4> count2 : {count2}</h4>
-    </h3>
-    <button key="btn1" onClick={()=>setCount(count+1)}>+</button>
-    {count}
-    {count >0 && '大于0'}
-    <button key="btn2"onClick={()=>setCount(count-1)}>-</button>
-  </div>
+
+  return (
+    <div id="test">
+      <h3>计数器: count {count >= 0 ? '>=' : '<'} 0</h3>
+      <button key="btn1" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+      {count}
+      {count > 0 && '大于0'}
+      <button key="btn2" onClick={() => setCount(count - 1)}>
+        -
+      </button>
+    </div>
+  )
 }
 
-function App () {
-  return <div>
-    <Counter count={1}/>
-    <footer>sreact</footer>
-  </div>
+function App() {
+  const [open, setSwitch] = useState(true)
+  return (
+    <div>
+      <button onClick={() => setSwitch(!open)}>
+        {open ? '当前：开' : '当前：关'}
+      </button>
+      {open && <Counter count={1} />}
+      <footer>sreact</footer>
+    </div>
+  )
 }
 render(<App key="root" />, document.getElementById('root')!)
