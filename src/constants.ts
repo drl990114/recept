b327dd7705779e0aeaff1b94c00f3310b00101d3
halodong <drl990114@gmail.com>
2022-06-07 19:24:06 +0800

    chore: add hook effect tag

--- src/constants.ts
+++ src/constants.ts
@@ -4,9 +4,14 @@ export const FunctionComponent /**      */ = 1
 export const HostComponent /**          */ = 2
 export const HostText /**               */ = 3
 
-// effect flag
+// fiber effect flag
 export const PLACEMENT /**              */ = Symbol.for('PLACEMENT')
 export const UPDATE /**                 */ = Symbol.for('UPDATE')
 export const DELETION /**               */ = Symbol.for('DELETION')
 
 export const ELEMENT_TEXT = '#text'
+
+// hook effect flag
+export const MOUNTEFFECT /**              */ = Symbol.for('MOUNTEFFECT')
+export const UNMOUNTEFFECT /**            */ = Symbol.for('UNMOUNTEFFECT')
+export const NOHOOKEFFECT /**             */ = Symbol.for('NOHOOKEFFECT')
