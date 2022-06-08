d95671da8157942b5e3e8f57e40513744287a6b6
halodong <drl990114@gmail.com>
2022-06-08 17:03:09 +0800

    chore: update example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -6,19 +6,46 @@ import {
   useMemo,
   useRef,
   useCallback,
+  useLayoutEffect,
 } from '../../src'
-import EffectDemo from "./components/useEffect"
-// import React, { useEffect, useState,useMemo,useRef ,useCallback} from 'react'
+import EffectDemo from './components/useEffect'
+// import React, {
+//   useEffect,
+//   useState,
+//   useMemo,
+//   useRef,
+//   useCallback,
+//   useLayoutEffect,
+// } from 'react'
 // import { render } from 'react-dom'
 
-
+export { h }
 export {
-  h,
   render,
   useState,
   useEffect,
   useMemo,
   useRef,
   useCallback,
+  useLayoutEffect,
 }
-render(<EffectDemo key="root" />, document.getElementById('root')!)
+
+function App(props) {
+  const [open, setIndex] = useState(true)
+
+  return (
+    <div>
+      <button onClick={() => setIndex(!open)}>
+        当前：{open ? '开' : '关'}
+      </button>
+      {open && (
+        <div>
+          <EffectDemo />
+          <h1>test</h1>
+        </div>
+      )}
+      <footer>recept</footer>
+    </div>
+  )
+}
+render(<App />, document.getElementById('root')!)
