d2d92d46916cfca95d89bbe104e7a3e5ac4e4119
halodong <drl990114@gmail.com>
2022-06-08 12:52:01 +0800

    chore: add h test case

--- __test__/h.spec.tsx
+++ __test__/h.spec.tsx
@@ -4,8 +4,8 @@ describe('h', () => {
   it('should resolve host components correctly', () => {
     const div = <div></div>
     expect(div).toEqual({
-      props: { children: [] },
       type: 'div',
+      props: { children: [] },
     })
   })
 
@@ -15,8 +15,66 @@ describe('h', () => {
     }
     expect(<App name="app" key="root" />).toEqual({
       key: 'root',
-      props: { key: 'root', name: 'app', children: [] },
       type: App,
+      props: { key: 'root', name: 'app', children: [] },
+    })
+  })
+
+  it('Special values should be parsed correctly', () => {
+    const div = (
+      <div>
+        {true}
+        {false}
+        {'bar'}
+        {null}
+      </div>
+    )
+    expect(div).toEqual({
+      type: 'div',
+      props: {
+        children: [
+          {
+            props: {
+              children: [],
+              text: 'bar',
+            },
+            type: '#text',
+          },
+        ],
+      },
+    })
+  })
+
+  it('Child nodes in array form should be expanded', () => {
+    const child = (props: any) => <li {...props}></li>
+    const childs = []
+
+    childs.push(child({ key: 'child1' }))
+    childs.push(child({ key: 'child2' }))
+
+    const ul = <ul>{childs}</ul>
+    expect(ul).toEqual({
+      type: 'ul',
+      props: {
+        children: [
+          {
+            type: 'li',
+            key: 'child1',
+            props: {
+              key: 'child1',
+              children: [],
+            },
+          },
+          {
+            type: 'li',
+            key: 'child2',
+            props: {
+              key: 'child2',
+              children: [],
+            },
+          },
+        ],
+      },
     })
   })
 })
