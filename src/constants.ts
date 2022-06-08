3414939ab7e24a5372a2b8f001cfa060e7a70f35
halodong <drl990114@gmail.com>
2022-06-08 12:38:21 +0800

    chore: formate code

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,17 @@
 // fiber worktags
-export const HostRoot /**               */ = 0
-export const FunctionComponent /**      */ = 1
-export const HostComponent /**          */ = 2
-export const HostText /**               */ = 3
-
-// fiber effect flag
-export const PLACEMENT /**              */ = Symbol.for('PLACEMENT')
-export const UPDATE /**                 */ = Symbol.for('UPDATE')
-export const DELETION /**               */ = Symbol.for('DELETION')
+export const HostRoot /**                 */ = 0
+export const FunctionComponent /**        */ = 1
+export const HostComponent /**            */ = 2
+export const HostText /**                 */ = 3
 
 export const ELEMENT_TEXT = '#text'
 
+// fiber effect flag
+export const PLACEMENT /**                */ = Symbol.for('PLACEMENT')
+export const UPDATE /**                   */ = Symbol.for('UPDATE')
+export const DELETION /**                 */ = Symbol.for('DELETION')
+
 // hook effect flag
-export const DEPENDEXEC /**              */ = Symbol.for('DEPENDEXEC')
-export const ONCE /**            */ = Symbol.for('ONCE')
+export const DEPENDEXEC /**               */ = Symbol.for('DEPENDEXEC')
+export const ONCE /**                     */ = Symbol.for('ONCE')
 export const NOHOOKEFFECT /**             */ = Symbol.for('NOHOOKEFFECT')
